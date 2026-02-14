a72075a80a97464188fda3d570cd808c6dd849f2
claudii21 <claudiadeazevedo@live.com.pt>
2026-02-14 23:21:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ colors: {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/do81earks/video/upload/v1771107250/Gravac%CC%A7a%CC%83o_nova_ocoli3.m4a", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/do81earks/video/upload/v1771107638/Gravac%CC%A7a%CC%83o_nova_uvixhk.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.7                        // Volume level (0.0 to 1.0)
