c4cffb4d932016171218ed43c7fec21e7d6dd58b
claudii21 <claudiadeazevedo@live.com.pt>
2026-02-14 22:52:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -67,10 +67,10 @@ colors: {
     // Animation settings
     // Adjust these if you want faster/slower animations
     animations: {
-        floatDuration: "10s",           // How long it takes hearts to float up (10-20s recommended)
-        floatDistance: "30px",          // How far hearts move sideways (30-70px recommended)
+        floatDuration: "20s",           // How long it takes hearts to float up (10-20s recommended)
+        floatDistance: "70px",          // How far hearts move sideways (30-70px recommended)
         bounceSpeed: "0.8s",            // Speed of bouncing animations (0.3-0.7s recommended)
-        heartExplosionSize: 1.9         // Size of heart explosion effect (1.2-2.0 recommended)
+        heartExplosionSize: 2.0         // Size of heart explosion effect (1.2-2.0 recommended)
     },
 
     // Background Music (Optional)
